752d69b224bb8b0bfb96663e1df5089a30442bfb
Demon-blood <64545469+Demon-blood@users.noreply.github.com>
2026-08-15 20:45:54 +0200

    fix Android provider authorization launcher import

--- android/lib/screens/va_operations_page.dart
+++ android/lib/screens/va_operations_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../app_state.dart';
 import '../theme/va_theme.dart';
